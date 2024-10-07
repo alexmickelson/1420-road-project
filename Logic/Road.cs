5139ccbec5947d3923b386e9dd4da2753d2547f9
Alex Mickelson <alexmickelson96@gmail.com>
2024-10-07 08:10:46 -0600

    workign demo

--- Logic/Road.cs
+++ Logic/Road.cs
@@ -3,10 +3,12 @@ using System.ComponentModel;
 public class Road
 {
 
-  public IVehicle[][] Vehicles { get; private set; }
+  public IVehicle?[][] Vehicles { get; private set; }
 
   public Road(int rowCount, int colCount)
   {
+    // initialize the first array to represent each row
+    // iterate through the first array with a for loop, initialize each position in the first array to be an array of IVehicles
     Vehicles = new IVehicle[rowCount][];
     for (int i = 0; i < rowCount; i++)
     {
@@ -19,7 +21,7 @@ public class Road
     Vehicles[row][column] = vehicle;
   }
 
-  public IVehicle GetVehicle(int row, int column)
+  public IVehicle? GetVehicle(int row, int column)
   {
     return Vehicles[row][column];
   }
